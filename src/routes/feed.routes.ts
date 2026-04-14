e0eef2cc6fe7b9b7b964c53940f07049a0439934
malat0o <rodrigo.malato2006@gmail.com>
2026-04-14 23:36:41 +0100

    Update feed.routes.ts

--- src/routes/feed.routes.ts
+++ src/routes/feed.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { getFeed } from '../controllers/feed.controller';
-import { authMiddleware } from '../middlewares/auth.middleware';
+import { authenticate } from '../middlewares/auth.middleware';
 
 const router = Router();
 
