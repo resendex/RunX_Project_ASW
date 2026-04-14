9bc98f92c982fa4bbe2ce7d14dba44eb7bea3483
malat0o <rodrigo.malato2006@gmail.com>
2026-04-14 23:37:41 +0100

    Update post.routes.ts

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { createPost, getPost, deletePost } from '../controllers/post.controller';
-import { authMiddleware } from '../middlewares/auth.middleware';
+import { authenticate } from '../middlewares/auth.middleware';
 
 const router = Router();
 
