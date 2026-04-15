3ec495813bc3d1307ca6bac2ad9bd7b20f52ef56
malat0o <rodrigo.malato2006@gmail.com>
2026-04-15 15:09:16 +0100

    Update import path for prisma in run.model.ts

--- src/models/run.model.ts
+++ src/models/run.model.ts
@@ -1,4 +1,4 @@
-import { prisma } from "../lib/prisma";
+import { prisma } from "../config/prisma";
 import type { CreateRunBody, UpdateRunBody } from "../schemas/run.schema";
 
 export interface RunFilters {
